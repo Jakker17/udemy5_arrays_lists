e96dcf98274b90aa4081d108204b822c3ebe4da7
Jakker17 <jurosmartin97@gmail.com>
2021-03-02 15:57:00 +0100

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,9 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -11,6 +15,64 @@ public class Main {
 
 
 
+ /*       String sentence= "Cwm fjord bank glyphs vext quiz";
 
+
+        for(int i=0;i<alphabet.length;i++){
+            if(sentence.toLowerCase().contains(String.valueOf(alphabet[i]))) System.out.println("true");
+            else System.out.println("false");
+        }
+
+
+
+        char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+        String[] help = new String[]{ "arp", "live", "strong" };
+
+        int index=-1;
+        for (int j=0;j<help[0].length();j++) {
+            char character =help[0].charAt(j);
+            for (int i = 0; i < alphabet.length; i++) {
+                if (character==alphabet[i])index =i+1;
+            }
+            System.out.println(index);
+        }
+*/
+
+
+
+        System.out.println(Arrays.toString(inArray(new String[]{ " ", "mice", "bull" },new String[] { "lively", "mice", "harp", "shar-p", "armstrong", "tarped ", "bullied" })));
+
+    }
+
+    public static int getIndex(char c){
+        int index =-1;
+        char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+
+        for (int i = 0; i < alphabet.length; i++) {
+            if (c==alphabet[i])index =i+1;
+        }
+return index;
+    }
+
+    public static String[] inArray(String[] array1, String[] array2){
+        if (array1.length==0)return null;
+
+        boolean tmp=false;
+        char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+        List<String> tmp2 = new ArrayList<>();
+        for (int i=0;i<array1.length;i++){
+         for (int j=0;j<array2.length;j++){
+             if (array1[i].equals(" ")) break;
+
+             if (array2[j].toLowerCase().contains(array1[i].toLowerCase()))
+             {
+                 tmp2.add(array1[i]);
+                    break;
+             }
+         }
+        }
+        String[] r = tmp2.toArray(new String[0]);
+        Arrays.sort(r);
+        return r;
     }
 }
--- src/com/company/MobilePhoneExercise/Contact.java
+++ src/com/company/MobilePhoneExercise/Contact.java
@@ -1,4 +1,24 @@
 package com.company.MobilePhoneExercise;
 
 public class Contact {
+    private String name, phoneNumber;
+
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public Contact createContact(String name, String phoneNumber){
+      return  new Contact(name,phoneNumber);
+    }
+
+
 }
